cf69b234337052fa69f7d2763a7e62ecdb8d3b51
Daniel Han <mrhanner@gmail.com>
2022-05-20 10:55:14 +0100

    update access

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports.generateRandomNumber = event => {
-  const randomNumber = parseInt(Math.random() * 100)
-  console.log("The randonm generated integer is:", randomNumber);
+  const randomNumber = parseInt(Math.random() * 100);
+  console.log("The random generated integer is:", randomNumber);
   return randomNumber;
 };
